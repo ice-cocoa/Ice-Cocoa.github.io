cb86c90a6cc3e8283ced5e15d8aed5277b9ba774
Ice-Cocoa <wangbonsgo@163.com>
2022-07-14 14:35:45 +0000

    commit

---
+++ js/simple-login.js
@@ -0,0 +1,18 @@
+
+
+
+while(true) {
+    var authorization = "";
+    authorization = prompt('authorization:', '');
+    if(authorization == 'wyc'){
+        break;
+    }else{
+        ;
+    }
+}
+
+
+
+
+
+
